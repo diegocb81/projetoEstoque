98b6dfdb44d87cae39b1186bc2e2e4feaaad0a8f
Diego Barcellos <diegocb81@gmail.com>
2021-07-01 23:04:16 -0300

    Encapsulando name, price e quantity

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,9 +6,7 @@ import java.util.Scanner;
 import entities.Product;
 
 public class Program {
-
 	public static void main(String[] args) {
-		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
@@ -18,6 +16,11 @@ public class Program {
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
 		Product product = new Product(name, price);
+		
+		product.setName("Computer");
+		System.out.println("Updated name: " + product.getName());
+		product.setPrice(1200.00);
+		System.out.println("Updated price: " + product.getPrice());
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
